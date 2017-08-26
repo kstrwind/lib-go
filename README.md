ab4e81610e0c8ba050b3d595ed175e2479b3e1a6
lijun <kstrwind@lijundeMacBook-Pro.local>
2017-08-26 17:00:22 +0800

    add kdebug

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # lib-go
-common lib 
+常用的Lib库
+
+## kdebug
+debug 工具
+
+### VarDump
+类似PHP的var_dump函数的debug函数，用于输出当前变量的类型及值， 特点如下：
+
+1. 递归地输出变量的所有成员类型及值
+2. 采用4空格来缩进，同一缩进级别表示同一层次的变量
+3. 对于指针，会在同一行解引用，显示指针指向的数据结构
+4. 对于数组、切片、映射、字符串等类型，会显示其长度
+5. 在切片、数组、映射结尾，会有结束标记 `//end $type` 
+
+*特殊标记说明：*
+
+- -->, 表示指针解引用
+- ==>, 表示数组、切片、映射、字符串等类型的值
+- :, 表示纯量变量或者结构体的成员等
+- (), 注释数组、切片、映射、字符串的长度
+
+*使用方法*
+```
+	var cfg *Conf = &Conf{}
+	err = yaml.Unmarshal(data, cfg)
+    if err != nil {
+        return err
+    }
+    kdebug.VarDump(cfg)
+
+输出：
+[*main.Conf] --> struct[main.Conf] ==> {
+    [*main.Person] --> struct[main.Person] ==> {
+        Name [string](6) ==> "peter"
+        Height [int] : 180
+        Weight [int] : 100
+    } \\end struct
+    slice[[]*main.Person](2) ==>
+        [*main.Person] --> struct[main.Person] ==> {
+            Name [string](2) ==> "p1"
+            Height [int] : 111
+            Weight [int] : 111
+        } \\end struct
+        [*main.Person] --> struct[main.Person] ==> {
+            Name [string](2) ==> "p2"
+            Height [int] : 222
+            Weight [int] : 222
+        } \\end struct
+    \\end slice
+} \\end struct
+```
---
+++ kdebug/debug.go
@@ -0,0 +1,118 @@
+package kdebug
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"runtime"
+)
+
+func VarDump(s interface{}) {
+	indent := ""
+
+	var_dump(reflect.ValueOf(s), indent, "")
+}
+
+func var_dump(value reflect.Value, indent string, prestr string) {
+	var vKind = value.Kind()
+	r_indent := indent
+	if prestr != "" {
+		r_indent = prestr
+	}
+
+	switch {
+	case vKind == 0:
+		fmt.Printf("%s[%s] : %v\n", r_indent, "nil", value)
+
+	//bool + int +uint
+	case vKind < 12:
+		fmt.Printf("%s[%s] : %v\n", r_indent, value.Type(), value)
+
+	//uintptr
+	case vKind == 12:
+		//fmt.Printf("%s[%s] ", r_indent, value.Type())
+		var_dump(value, indent, fmt.Sprintf("%s[%s] --> ", indent, value.Type()))
+
+	//float + complex
+	case vKind < 17:
+		fmt.Printf("%s[%s] : %v\n", r_indent, value.Type(), value)
+
+	//Array
+	case vKind == 17:
+		fmt.Printf("%sarray[%s](%d) ==>\n", r_indent, value.Type(), value.Len())
+		index := 0
+		for {
+			if index >= value.Len() {
+				break
+			}
+			//fmt.Printf("%s[%d] :", r_indent+"    ", index)
+			var_dump(value.Index(index), "    "+indent, fmt.Sprintf("%s[%d] :", r_indent+"    ", index))
+			index++
+		}
+
+	//chan\func\Interface
+	case vKind < 21:
+		fmt.Printf("%s[%s]%v", r_indent, value.Type(), value)
+
+	//Map
+	case vKind == 21:
+		fmt.Printf("%smap[%s](%d) ==>\n", r_indent, value.Type(), value.Len())
+		keys := value.MapKeys()
+		for _, key := range keys {
+			//fmt.Printf("%s[%s] : ", indent+"    ", key)
+			var_dump(value.MapIndex(key), "    "+indent, fmt.Sprintf("%s[%s] : ", indent+"    ", key))
+		}
+
+	//ptr
+	case vKind == 22:
+		//fmt.Printf("%s[%s] -->\n", indent, value.Type())
+		var_dump(reflect.Indirect(value), indent, fmt.Sprintf("%s[%s] --> ", indent, value.Type()))
+
+	//slice
+	case vKind == 23:
+		fmt.Printf("%sslice[%s](%d) ==>\n", indent, value.Type(), value.Len())
+		//fmt.Printf("%s%s", indent+"    ", t.Field(k).Name)
+		index := 0
+		for {
+			if index >= value.Len() {
+				break
+			}
+			//fmt.Printf("%s[%d] : ", r_indent+"    ", index)
+			var_dump(value.Index(index), "    "+indent, fmt.Sprintf("%s[%d] : ", indent+"    ", index))
+			index++
+		}
+		fmt.Printf("%s\\\\end %s\n", indent, vKind.String())
+
+	//string
+	case vKind == 24:
+		fmt.Printf("%s[%s](%d) ==> \"%s\"\n", r_indent, value.Type(), value.Len(), value)
+
+	//Struct
+	case vKind == 25:
+		fmt.Printf("%sstruct[%s] ==> {\n", r_indent, value.Type())
+		t := value.Type()
+		for k := 0; k < t.NumField(); k++ {
+			var_dump(value.Field(k), indent+"    ", fmt.Sprintf("%s%s ", indent+"    ", t.Field(k).Name))
+		}
+		fmt.Printf("%s} \\\\end %s\n", indent, vKind.String())
+
+	//Unsafeptr
+	case vKind == 26:
+		//fmt.Printf("%s[%s] -->\n", r_indent, vKind.String())
+		var_dump(reflect.Indirect(value), indent, fmt.Sprintf("%s[%s] --> ", indent, vKind.String()))
+
+	default:
+		fmt.Printf("%s[%s] : %v\n", r_indent, "Unknown", value)
+	}
+
+	return
+}
+
+func GetFuncName() (string, error) {
+	pc, _, _, succ := runtime.Caller(1)
+	if !succ {
+		return "", errors.New("get current function name failed")
+	}
+
+	return runtime.FuncForPC(pc).Name(), nil
+}
