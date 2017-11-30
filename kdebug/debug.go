af01ae08759065044d28ff8c356eb76d6c0fc98c
李俊 <lijun@lvwan.com>
2017-11-30 21:44:13 +0800

    修复传入 reflect.Value类型的值会导致无限递归，进而导致栈溢出的问题

--- kdebug/debug.go
+++ kdebug/debug.go
@@ -9,64 +9,70 @@ import (
 
 func VarDump(s interface{}) {
 	indent := ""
-
-	var_dump(reflect.ValueOf(s), indent, "")
+	switch s.(type) {
+	case reflect.Value:
+		varDump(s.(reflect.Value), indent, "")
+	default:
+		varDump(reflect.ValueOf(s), indent, "")
+	}
+	//varDump(reflect.ValueOf(s), indent, "")
 }
 
-func var_dump(value reflect.Value, indent string, prestr string) {
+func varDump(value reflect.Value, indent string, preStr string) {
 	var vKind = value.Kind()
-	r_indent := indent
-	if prestr != "" {
-		r_indent = prestr
+	rIndent := indent
+	if preStr != "" {
+		rIndent = preStr
 	}
 
 	switch {
 	case vKind == 0:
-		fmt.Printf("%s[%s] : %v\n", r_indent, "nil", value)
+		fmt.Printf("%s[%s] : %v\n", rIndent, "nil", value)
 
 	//bool + int +uint
 	case vKind < 12:
-		fmt.Printf("%s[%s] : %v\n", r_indent, value.Type(), value)
+		fmt.Printf("%s[%s] : %v\n", rIndent, value.Type(), value)
 
 	//uintptr
 	case vKind == 12:
 		//fmt.Printf("%s[%s] ", r_indent, value.Type())
-		var_dump(value, indent, fmt.Sprintf("%s[%s] --> ", indent, value.Type()))
+
+		varDump(value, indent, fmt.Sprintf("%s[%s] --> ", indent, value.Type()))
 
 	//float + complex
 	case vKind < 17:
-		fmt.Printf("%s[%s] : %v\n", r_indent, value.Type(), value)
+		fmt.Printf("%s[%s] : %v\n", rIndent, value.Type(), value)
 
 	//Array
 	case vKind == 17:
-		fmt.Printf("%sarray[%s](%d) ==>\n", r_indent, value.Type(), value.Len())
+		fmt.Printf("%sarray[%s](%d) ==>\n", rIndent, value.Type(), value.Len())
 		index := 0
 		for {
 			if index >= value.Len() {
 				break
 			}
 			//fmt.Printf("%s[%d] :", r_indent+"    ", index)
-			var_dump(value.Index(index), "    "+indent, fmt.Sprintf("%s[%d] :", r_indent+"    ", index))
+			varDump(value.Index(index), "    "+indent, fmt.Sprintf("%s[%d] :", rIndent+"    ", index))
 			index++
 		}
 
 	//chan\func\Interface
 	case vKind < 21:
-		fmt.Printf("%s[%s]%v", r_indent, value.Type(), value)
+		fmt.Printf("%s[%s]%v", rIndent, value.Type(), value)
 
 	//Map
 	case vKind == 21:
-		fmt.Printf("%smap[%s](%d) ==>\n", r_indent, value.Type(), value.Len())
+		fmt.Printf("%smap[%s](%d) ==>\n", rIndent, value.Type(), value.Len())
 		keys := value.MapKeys()
 		for _, key := range keys {
 			//fmt.Printf("%s[%s] : ", indent+"    ", key)
-			var_dump(value.MapIndex(key), "    "+indent, fmt.Sprintf("%s[%s] : ", indent+"    ", key))
+			varDump(value.MapIndex(key), "    "+indent, fmt.Sprintf("%s\"%s\" : ", indent+"    ", key))
 		}
 
 	//ptr
 	case vKind == 22:
 		//fmt.Printf("%s[%s] -->\n", indent, value.Type())
-		var_dump(reflect.Indirect(value), indent, fmt.Sprintf("%s[%s] --> ", indent, value.Type()))
+		varDump(reflect.Indirect(value), indent, fmt.Sprintf("%s[%s] --> ", indent, value.Type()))
 
 	//slice
 	case vKind == 23:
@@ -78,31 +84,32 @@ func var_dump(value reflect.Value, indent string, prestr string) {
 				break
 			}
 			//fmt.Printf("%s[%d] : ", r_indent+"    ", index)
-			var_dump(value.Index(index), "    "+indent, fmt.Sprintf("%s[%d] : ", indent+"    ", index))
+			varDump(value.Index(index), "    "+indent, fmt.Sprintf("%s[%d] : ", indent+"    ", index))
 			index++
 		}
 		fmt.Printf("%s//end %s\n", indent, vKind.String())
 
 	//string
 	case vKind == 24:
-		fmt.Printf("%s[%s](%d) ==> \"%s\"\n", r_indent, value.Type(), value.Len(), value)
+		fmt.Printf("%s[%s](%d) ==> \"%s\"\n", rIndent, value.Type(), value.Len(), value)
 
 	//Struct
 	case vKind == 25:
-		fmt.Printf("%sstruct[%s] ==> {\n", r_indent, value.Type())
+		fmt.Printf("%sstruct[%s] ==> {\n", rIndent, value.Type())
+
 		t := value.Type()
-		for k := 0; k < t.NumField(); k++ {
-			var_dump(value.Field(k), indent+"    ", fmt.Sprintf("%s%s ", indent+"    ", t.Field(k).Name))
+		for k := 0; k < value.NumField(); k++ {
+			varDump(value.Field(k), indent+"    ", fmt.Sprintf("%s%s ", indent+"    ", t.Field(k).Name))
 		}
 		fmt.Printf("%s} //end %s\n", indent, vKind.String())
 
 	//Unsafeptr
 	case vKind == 26:
 		//fmt.Printf("%s[%s] -->\n", r_indent, vKind.String())
-		var_dump(reflect.Indirect(value), indent, fmt.Sprintf("%s[%s] --> ", indent, vKind.String()))
+		varDump(reflect.Indirect(value), indent, fmt.Sprintf("%s[%s] --> ", indent, vKind.String()))
 
 	default:
-		fmt.Printf("%s[%s] : %v\n", r_indent, "Unknown", value)
+		fmt.Printf("%s[%s] : %v\n", rIndent, "Unknown", value)
 	}
 
 	return
